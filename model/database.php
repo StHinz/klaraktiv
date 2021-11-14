e4c554fa26c2d423c1238712f7cf378b461c3d0f
StHinz <39180199+StHinz@users.noreply.github.com>
2021-11-14 11:18:53 +0100

    db conn

--- model/database.php
+++ model/database.php
@@ -5,13 +5,13 @@ class database {
     protected $datab;
 
 // DB Connection - Port 3308, User etc.
-public function __construct($username = "root", $password = "123456", 
+public function __construct($username = "root", $password = "", 
 $host = "127.0.0.1", $dbname = "klaraktiv", $options = []) {
 
     $this->isConn = TRUE;
 
     try {
-        $this->datab = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8;port=3306', $username, $password, $options);
+        $this->datab = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8;port=3308', $username, $password, $options);
         $this->datab->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->datab->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
