8954b12276f826e4147fd78bfe497b250840563c
StHinz <39180199+StHinz@users.noreply.github.com>
2022-03-06 14:47:44 +0100

    add phpqr and scanner to lib

---
+++ example.php
@@ -0,0 +1,38 @@
+<html>
+    <Head><title></title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+
+</Head>
+<body>
+
+
+<video id="preview"></video>
+
+<input type="text" name="text" id="text" class="form-control">
+
+<script type="text/javascript">
+  let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+    scanner.addListener('scan', function (content) {
+        document.getElementById("text").value=content;
+
+  }); 
+  Instascan.Camera.getCameras().then(function (cameras) {
+	if (cameras.length > 0) {
+	  scanner.start(cameras[0]);
+	} else {
+	  console.error('No cameras found.');
+	}
+  }).catch(function (e) {
+	console.error(e);
+  });
+
+
+  
+</script>
+
+
+
+</body>
+
+</html>
